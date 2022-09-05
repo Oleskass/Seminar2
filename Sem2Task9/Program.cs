e5e732b41da4f4156c99c2233c9f2059d4201ac1
Olesya Solovyova <soe611@yandex.ru>
2022-09-05 18:53:27 +0300

    Task9 First decision of Task

---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,23 @@
+﻿//===========================================================
+// #9 Напишите программу, которая выводит случайное число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+// Например: 78 -> 8, 12-> 2, 85 -> 8
+//===========================================================
+
+System.Random numberGenerator = new System.Random();
+int number = numberGenerator.Next(9, 100);
+//int number = new Random().Next (9,100); //это то же самое, но в одну строку
+
+Console.WriteLine(number);
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
