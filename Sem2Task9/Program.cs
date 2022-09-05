0d19188f774e9e4fec45203ba2131fafef95cd19
Olesya Solovyova <soe611@yandex.ru>
2022-09-05 19:53:09 +0300

    Task9 Random 10-99 and show max digit

--- Sem2Task9/Program.cs
+++ Sem2Task9/Program.cs
@@ -4,20 +4,46 @@
 // Например: 78 -> 8, 12-> 2, 85 -> 8
 //===========================================================
 
-System.Random numberGenerator = new System.Random();
-int number = numberGenerator.Next(9, 100);
-//int number = new Random().Next (9,100); //это то же самое, но в одну строку
+version1(); //if-else
+version2(); //ternary operator
+version3(); //char
 
-Console.WriteLine(number);
-
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-
-if (firstDigit > secondDigit)
+void version1() //if-else
 {
-    Console.WriteLine(firstDigit);
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(9, 100);
+    //int number = new Random().Next (9,100); //это то же самое, но в одну строку
+    Console.WriteLine("Метод 1:\n" + number);
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    if (firstDigit > secondDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(secondDigit);
+    }
 }
-else
+void version2() //ternary operator
 {
-    Console.WriteLine(secondDigit);
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(9, 100);
+    Console.WriteLine("Метод 2:\n" + number);
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+
+    Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+}
+void version3() //char
+{
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(9, 100);
+    Console.WriteLine("Метод 3:\n" + number);
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    char[] charArray = number.ToString().ToCharArray();
+
+    Console.WriteLine((int)charArray[0] > (int)charArray[1] ? charArray[0] : charArray[1]);
 }
